9f835dc1d2aac3a620d0c29f5cbb6090e5ea2014
CARLOS MARIO ALBA RODRIGUEZ <cm.alba10@sis.virtual.uniandes.edu.co>
2019-04-02 19:01:20 -0500

    terminado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//import {IntlProvider} from 'react-intl';
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+import { IntlProvider, addLocaleData } from 'react-intl';
+import esLocaleData from 'react-intl/locale-data/es';
+import enLocaleData from 'react-intl/locale-data/en';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+
+var userLang = navigator.language || navigator.userLanguage;
+
+//alert ("The language is: " + userLang);
+
+
+
+if (userLang == 'es-ES') {
+    //Español
+    addLocaleData(esLocaleData);
+
+    ReactDOM.render(
+        <IntlProvider locale="es-ES" messages={localeEsMessages}>
+            <JobsList />
+        </IntlProvider>, document.getElementById("root")
+    );
+
+}
+else {
+    //English
+    addLocaleData(enLocaleData);
+
+    ReactDOM.render(
+        <IntlProvider locale="en-EN" messages={localeEnMessages}>
+            <JobsList />
+        </IntlProvider>, document.getElementById("root")
+    );
+
+}
+
+
+
+
+
+
